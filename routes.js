25e82b6542feb7a24df2898d5e11c3b7f9076791
rozisaputra24 <82568840+rozisaputra24@users.noreply.github.com>
2023-05-29 16:29:11 +0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,29 +1,16 @@
 const express = require('express');
+const { getPenyakitByName } = require('./handler');
+
 const router = express.Router();
-const { getPenyakitDetail, getPenyakitById, getPenyakitByNama } = require('./handler');
 
-router.get('/penyakit', (req, res) => {
-    const penyakitDetail = getPenyakitDetail();
-    res.json(penyakitDetail);
-});
+router.get('/penyakit/:nama', (req, res) => {
+    const { nama } = req.params;
 
-router.get('/penyakit/id/:id', (req, res) => {
-    const id = req.params.id;
-    const penyakit = getPenyakitById(id);
-    if (penyakit) {
+    try {
+        const penyakit = getPenyakitByName(nama);
         res.json(penyakit);
-    } else {
-        res.status(404).send('Penyakit not found');
-    }
-});
-
-router.get('/penyakit/nama/:nama', (req, res) => {
-    const nama = req.params.nama;
-    const penyakit = getPenyakitByNama(nama);
-    if (penyakit) {
-        res.json(penyakit);
-    } else {
-        res.status(404).send('Penyakit not found');
+    } catch (error) {
+        res.status(404).json({ error: error.message });
     }
 });
 
