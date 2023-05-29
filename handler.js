8c95223fa27acefb9405587f30a1b346eb68c1c7
rozisaputra24 <82568840+rozisaputra24@users.noreply.github.com>
2023-05-29 17:52:39 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,15 +1,11 @@
 const data = require('./data.json');
 
-const getPenyakitByName = (nama) => {
-    const penyakit = data.penyakit.find((item) => item.hasOwnProperty(nama));
+function getPenyakit(nama) {
+  const penyakit = data.penyakit.find(item => item.nama.toLowerCase() === nama.toLowerCase());
 
-    if (penyakit) {
-        return penyakit[nama];
-    } else {
-        throw new Error('Penyakit tidak ditemukan');
-    }
-};
+  return penyakit;
+}
 
 module.exports = {
-    getPenyakitByName,
+  getPenyakit
 };
