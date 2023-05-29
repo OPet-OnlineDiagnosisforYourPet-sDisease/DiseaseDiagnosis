57a8d9f79367275a035db2f48ff5b64a1e630212
rozisaputra24 <82568840+rozisaputra24@users.noreply.github.com>
2023-05-29 19:16:46 +0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,18 +1,8 @@
 const express = require('express');
-const { getPenyakitByName } = require('./handler');
-
 const router = express.Router();
+const handler = require('./handler');
 
-router.get('/penyakit/:nama', (req, res) => {
-  let { nama } = req.params;
-  nama = nama.toLowerCase(); // Mengubah nama menjadi huruf kecil
-
-  try {
-    const penyakit = getPenyakitByName(nama);
-    res.json(penyakit);
-  } catch (error) {
-    res.status(404).json({ error: error.message });
-  }
-});
+router.get('/penyakit', handler.getPenyakit);
+router.get('/penyakit/:nama', handler.getDetailPenyakitIgnoreCase);
 
 module.exports = router;
